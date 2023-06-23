b75c4dbf9879dd710fd59aaff350b283622f9c19
Stanislav <Stas.zaliiski@gmail.com>
2023-06-23 20:18:18 +0200

    main

--- App.js
+++ App.js
@@ -1,31 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import LoginScreen from "./Screens/Auth/LoginScreen/LoginScreen";
-import { useFonts } from "expo-font";
+
 // import RegistrationScreen from "./Screens/Auth/RegistrationScreen/RegistrationScreen";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
-    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
   return (
     <View>
-      <LoginScreen style={styles.container} />
+      <LoginScreen />
       {/* <RegistrationScreen /> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- Screens/Auth/LoginScreen/LoginScreen.js
+++ Screens/Auth/LoginScreen/LoginScreen.js
@@ -1,63 +1,51 @@
 import { ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { commonStyle } from "../../../styles/commonStyle";
-import { authStyles as s } from "../authStyles";
 
 export default function LoginScreen() {
   return (
-    <>
-      <View style={{ flex: 1 }}>
-        <ImageBackground style={styles.bg} source={require("../../../image/background.jpg")}>
-          <View style={styles.inner}>
-            <Text style={commonStyle.title}>Увійти</Text>
-            <View style={s.inputWrapper}>
-              <TextInput
-                style={s.input}
-                autoComplete="email"
-                keyboardType="email-address"
-                textContentType="emailAddress"
-                placeholder="Адреса електроної пошти"
-                value=""
-              />
-            </View>
-            <View style={s.inputWrapper}>
-              <View style={{ flex: 4 }}>
-                <TextInput style={s.input} placeholder="Пароль" value="" />
-              </View>
-              <View>
-                <TouchableOpacity style={s.btnInput}>
-                  <Text style={s.btnInputText}>Показати</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-
-            {/* <>
-              <View style={{ marginBottom: 16 }}>
-                <Btn text="Войти" />
-              </View>
-
-              <Text style={s.text}>
-                Немає акаунта? <Text>Зареєструватись</Text>
-              </Text>
-            </> */}
+    <View style={styles.container}>
+      <ImageBackground style={styles.bg} source={require("../../../image/background.jpg")}>
+        <View>
+          <Text>Увійти</Text>
+          <View>
+            <TextInput
+              autoComplete="email"
+              keyboardType="email-address"
+              textContentType="emailAddress"
+              placeholder="Адреса електроної пошти"
+              value=""
+            />
           </View>
-        </ImageBackground>
-      </View>
-    </>
+          <View>
+            <View>
+              <TextInput placeholder="Пароль" value="" />
+            </View>
+            <View>
+              <TouchableOpacity>
+                <Text>Показати</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          {/* <>
+            <View>
+              <Btn text="Войти" />
+            </View>
+
+            <Text style={s.text}>
+              Немає акаунта? <Text>Зареєструватись</Text>
+            </Text>
+          </> */}
+        </View>
+      </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   bg: {
     flex: 1,
     resizeMode: "cover",
-    justifyContent: "flex-end",
-  },
-  inner: {
-    paddingTop: 32,
-    paddingHorizontal: 16,
-    paddingBottom: 144,
-    backgroundColor: "#ffffff",
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
   },
 });
