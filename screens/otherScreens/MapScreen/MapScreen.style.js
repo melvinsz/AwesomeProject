12305eff09e0473c1524380f36535d931e1f60d2
Stanislav <Stas.zaliiski@gmail.com>
2023-08-21 22:29:22 +0200

    hw5

--- screens/otherScreens/MapScreen/MapScreen.style.js
+++ screens/otherScreens/MapScreen/MapScreen.style.js
@@ -1,10 +1,14 @@
-import { StyleSheet } from "react-native";
+// import { StyleSheet, Dimensions } from "react-native";
 
-export const styles = StyleSheet.create({
-  mapContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FFFFFF",
-  },
-});
+// export const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   map: {
+//     width: Dimensions.get("window").width,
+//     height: Dimensions.get("window").height,
+//   },
+// });
